7bcfbd38b4ad79b2f2af105adfef71b6f2d6c422
Stefan-RR <stefan.ruchlewicz@outlook.com>
2023-01-09 13:19:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Coin from './components/Coin';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <span className='content'>
+
+      </span>
+    </div>
+  );
+}
+
+export default App;
