195b1747e15235ed9b0de7d2c8a21e5fe01279db
Stefan-RR <stefan.ruchlewicz@outlook.com>
2023-01-09 14:01:19 -0700

    Began page structure and styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className='App'>
       <span className='content'>
-
+        <Coin />
+        <Coin />
       </span>
     </div>
   );
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Coin = () => {
   return (
-    <div>Coin</div>
+    <div>
+        <input type='text' className='coin'/>
+    </div>
   )
 }
 
