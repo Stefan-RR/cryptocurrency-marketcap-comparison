6b4bf823ea7f452ce798a91d0c46ccd37aeb3c8c
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-09 13:13:15 -0700

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function Coin({ selectedCoin, onChangeCoin }) {
+function Coin({ selectedCoin, onChangeCoin, coinOptions }) {
   return (
     <div>
-        <input type='text' className='coin'/>
         <select id='selected' value={selectedCoin} onChange={onChangeCoin}>
-
+          {coinOptions.map(option => (
+            <option value={option}>{option}</option>
+          ))}
         </select>
     </div>
   )
