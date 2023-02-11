514e6c6f3185e7e38f0cc277813f9c3408b3ff20
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-10 18:46:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect} from 'react';
 import Coin from './components/Coin';
 import './App.css';
 
 function App() {
+
+  const [coinOptions, setCoinOptions] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => res.json())
+    .then(data => {
+      setCoinOptions([data.name, ...Object.keys(data.market_cap)])
+      console.log(setCoinOptions)
+    })
+  }, [])
+
   return (
     <div className='App'>
       <span className='content'>
+        <Coin
+          coinOptions={coinOptions}
+        />
         <Coin />
+        <Coin
+          coinOptions={coinOptions}
+        />
         <Coin />
       </span>
     </div>
