e862455d6a9ddaa663e5566b18ee9411ad37cb2a
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-12 16:20:33 -0700

    Finished price conversion

--- src/App.js
+++ src/App.js
@@ -6,29 +6,37 @@ function App() {
 
   const [fromCoin, setFromCoin] = useState();
   const [toCoin, setToCoin] = useState();
-  const [coinList, setCoinList] = useState([]);
-
+  const [fromCoinList, setFromCoinList] = useState([]);
+  const [toCoinList, setToCoinList] = useState([]);
 
   useEffect(() => {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => res.json())
     .then(data => {
-     setCoinList(data.map(data => <option value={data.id}>{data.name}</option>))
+     setFromCoinList(data.map(data => <option key={data.id} value={data.market_cap}>{data.name}</option>))
+     setToCoinList(data.map(data => <option key={data.id} value={data.circulating_supply}>{data.name}</option>))
+
     })
   }, [])
 
+  var mCap = parseInt(fromCoin)
+  var supply = parseInt(toCoin)
+  var newPrice = mCap/supply
+  
   return (
     <div className='App'>
       <span className='content'>
         <Coin
           selectedCoin={fromCoin}
-          coinOptions={coinList}
+          onChangeCoin={e => setFromCoin(e.target.value)}
+          coinOptions={fromCoinList}
         />
          <Coin
           selectedCoin={toCoin}
-          coinOptions={coinList}
+          onChangeCoin={e => setToCoin(e.target.value)}
+          coinOptions={toCoinList}
         />
-        
+        <p>{newPrice}</p>
       </span>
     </div>
   );
