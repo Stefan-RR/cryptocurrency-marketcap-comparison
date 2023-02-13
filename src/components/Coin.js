ddf00882ca0d04f0e344a39a4248762a3a4ce0b6
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-13 14:35:15 -0700

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -4,6 +4,7 @@ function Coin({ selectedCoin, onChangeCoin, coinOptions }) {
   return (
     <div>
         <select className='selected' value={selectedCoin} onChange={onChangeCoin}>
+          <option value=''>Select a Cryptocurrency</option>
           {coinOptions}
         </select>
     </div>
